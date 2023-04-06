d13693df1293d082b9b97a8784bbd4b1765c9703
Mark24 <mark.zhangyoung@gmail.com>
2023-04-06 23:37:27 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# auto-deploy-rime
+# Rime auto deploy
+
+仅在MacOS下测试
+
+# 自动部署内容：
+
+## step1: 确认安装 Rime 输入法，自动安装
+
+需要用户自行登出，重进系统，设置Rime输入法为系统输入法
+
+## step2: 备份 Rime 默认配置
+
+## step3: 自动安装 Rime-ice 配置
+
+## step4: 自动追加自定义配置模板
---
+++ deploy.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+puts "==== Rime auto deploy ===="
+puts "Have you installed Rime?(y/n)"
+
+result = gets
+if result.strip != "y"
+  puts "(1/4) Install rime ? (y/n)"
+  result = gets
+  system("brew install --cask squirrel") if result.strip == "y"
+  puts "step1: Logout and relogin system"
+  puts "step2: Make Rime Input as your input method in system pane."
+  puts "step3: Rerun this program, jump over install Rime step."
+else
+  puts "Do you want to continue?(y/n)"
+  result = gets
+  exit 0 if result.strip != "y"
+
+  puts "(2/4) Backup Rime default config."
+  puts "run `mv ~/Library/Rime ~/Library/Rime.old`"
+  system("mv ~/Library/Rime ~/Library/Rime.old")
+  puts "done ✅"
+  puts ""
+  puts "(3/4) Install Rime-ice config."
+  puts "run `git clone --depth=1 https://github.com/Mark24Code/rime-ice.git ~/Library/Rime`"
+  system(
+    "git clone --depth=1 https://github.com/Mark24Code/rime-ice.git ~/Library/Rime"
+  )
+  puts "done ✅"
+  puts ""
+  puts "(4/4) Download Custom config."
+  system("cp ./default.custom.yaml ~/Library/Rime/")
+  system("cp ./squirrel.custom.yaml ~/Library/Rime/")
+  puts "done ✅"
+  puts ""
+  puts "Deploy finised"
+  puts "Please open Rime setting pane and click `redeploy`. Enjoy~ 🍻"
+end
